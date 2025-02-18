34645e89811ce30e420cc78baa0698464eb03497
b9na <111341708+b9natwo@users.noreply.github.com>
2025-02-17 23:30:05 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,25 +1,34 @@
-document.getElementById('krakenForm').addEventListener('submit', function(event) {
+document.getElementById('fileForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
     // Get the input value
-    const krakenLink = document.getElementById('krakenLink').value;
+    const fileLink = document.getElementById('fileLink').value;
     
-    // Extract the ID from the KrakenFiles link
-    const regex = /krakenfiles\.com\/view\/([a-zA-Z0-9]+)\/file\.html/;
-    const match = krakenLink.match(regex);
+    // Extract the ID from the KrakenFiles or plwcse.top link
+    const krakenRegex = /krakenfiles\.com\/view\/([a-zA-Z0-9]+)\/file\.html/;
+    const plwcseRegex = /plwcse\.top\/f\/([a-zA-Z0-9]+)/;
+    const pillowcaseRegex = /pillowcase\.su\/f\/([a-zA-Z0-9]+)/;
     
-    if (match && match[1]) {
-        const id = match[1];
-        
-        // Create the new embed link
-        const newLink = `https://krakenfiles.com/embed-audio/${id}?autoplay=false&link=true`;
-        
-        // Update the shareable link
-        const shareLink = `${window.location.origin}/view/${id}`;
-        document.getElementById('shareLinkUrl').href = shareLink;
-        document.getElementById('shareLinkUrl').textContent = shareLink;
-        document.getElementById('shareLink').style.display = 'block';
+    const krakenMatch = fileLink.match(krakenRegex);
+    const plwcseMatch = fileLink.match(plwcseRegex);
+    const pillowcaseMatch = fileLink.match(pillowcaseRegex)
+    
+    let id, shareLink;
+    if (krakenMatch && krakenMatch[1]) {
+        id = krakenMatch[1];
+        shareLink = `${window.location.origin}/view/${id}`;
+    } else if (plwcseMatch && plwcseMatch[1]) {
+        id = plwcseMatch[1];
+        shareLink = `${window.location.origin}/f/${id}`;
+    } else if (pillowcaseMatch && pillowcaseMatch[1]) {
+        id = pillowcaseMatch[1];
+        shareLink = `${window.location.origin}/f/${id}`;
     } else {
-        alert('Invalid KrakenFiles link');
+        alert('Invalid link');
+        return;
     }
+    
+    document.getElementById('shareLinkUrl').href = shareLink;
+    document.getElementById('shareLinkUrl').textContent = shareLink;
+    document.getElementById('shareLink').style.display = 'block';
 });
